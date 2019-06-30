bcb20ef4dca6a43cca401b7ee8fb0ac3fc1f493e
Vitor Maracajá <vitorvmrs@hotmail.com>
2019-06-30 10:27:35 -0300

    Finished

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="">
+        <b-nav tabs>
+            <b-nav-item disabled>
+                <strong>Quiz App =]</strong>
+            </b-nav-item>
+            <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
+        </b-nav>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: [
+            'numCorrect',
+            'numTotal'
+        ]
+    }
+</script>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,135 @@
+<template>
+    <div>
+        <b-jumbotron class="mt-5">
+
+            <template slot="lead">
+                {{ currentQuestion.question }}
+            </template>
+
+            <hr class="my-4">
+
+            <b-list-group>
+                <b-list-group-item
+                    v-for="(answer, index) in answers"
+                    :key="index"
+                    @click.prevent="selectAnswer(index)"
+                    :class="answerClass(index)"
+                >
+                    {{ answer }}
+                </b-list-group-item>
+            </b-list-group>
+
+            <b-button
+                variant="primary"
+                @click="submitAnswer"
+                :disabled="selectedIndex === null || answered"
+            >
+                Submit
+            </b-button>
+            <b-button @click="next" variant="success">
+                Next >
+            </b-button>
+        </b-jumbotron>
+    </div>
+</template>
+
+<script>
+    import _ from 'lodash';
+
+    export default {
+        props: {
+            currentQuestion: Object,
+            next: Function,
+            increment: Function,
+        },
+        data() {
+            return {
+                selectedIndex: null,
+                correctIndex: null,
+                shuffledAnswers: [],
+                answered: false,
+            }
+        },
+        computed: {
+            answers() {
+                let answers = this.shuffledAnswers;
+                return answers;
+            }
+        },
+        watch: {
+            currentQuestion: {
+                immediate: true,
+                handler() {
+                    this.selectedIndex = null;
+                    this.answered = false;
+                    this.shuffleAnswers();
+                }
+            }
+        },
+        methods: {
+            selectAnswer(index) {
+                this.selectedIndex = index;
+                console.log('index -> ', index);
+            },
+            submitAnswer() {
+                let isCorrect = false;
+
+                if (this.selectedIndex === this.correctIndex ) {
+                    isCorrect = true;
+                }
+
+                this.answered = true;
+
+                this.increment(isCorrect);
+            },
+            shuffleAnswers() {
+                let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer];
+                this.shuffledAnswers = _.shuffle(answers);
+                this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer);
+            },
+            answerClass(index) {
+                let answerClass = '';
+
+                if (!this.answered && this.selectedIndex === index) {
+                    answerClass = 'selected';
+                } else if (this.answered && this.correctIndex === index) {
+                    answerClass = 'correct';
+                } else if (this.answered &&
+                    this.selectedIndex === index &&
+                    this.correctIndex !== index
+                ) {
+                    answerClass = 'incorrect';
+                }
+
+                return answerClass;
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .list-group {
+        margin-bottom: 15px;
+    }
+
+    .list-group-item:hover {
+        background-color: #eeeeee;
+        cursor: pointer;
+    }
+
+    .btn {
+        margin: 0 5px;
+    }
+
+    .selected {
+        background-color: lightblue;
+    }
+
+    .correct {
+        background-color: lightgreen;
+    }
+
+    .incorrect {
+        background-color: red;
+    }
+</style>
